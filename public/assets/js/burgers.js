7da802c8797e300103f2a8d2b47c79b7c175c0ae
texas0418 <simonshih@hotmail.com>
2019-03-15 00:27:13 -0500

    fixed issues with buttons not working

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,41 +1,30 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
+$(document).ready(function() {
   $(".devoured").on("click", function(event) {
     var id = $(this).data("id");
     var newDevour = $(this).data("newdevour");
-
     var newDevourState = {
-      devour: newDevour
+      devoured: newDevour
     };
-
-    // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: newDevourState
-    }).then(
-      function() {
-        location.reload();
-      }
-    );
+    }).then(function() {
+      location.reload();
+    });
   });
 
   $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
     event.preventDefault();
-
     var newBurger = {
-      name: $("#burger").val().trim(),
-      devoured: $("[name=devoured]:checked").val().trim()
+      name: $("#burger")
+        .val()
+        .trim()
     };
-
-    // Send the POST request.
-    $.ajax("/api/burgers", {
+    $.ajax("/api/burger", {
       type: "POST",
       data: newBurger
-    }).then(
-      function() {
-        location.reload();
-      }
-    );
+    }).then(function() {
+      location.reload();
+    });
   });
 });
